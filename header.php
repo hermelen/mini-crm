7449d0db4a21c73bd90e49c22b553eb55723e26f
hermelen <hermelen.peris@gmail.com>
2018-08-07 09:19:05 +0200

    initial mini-crm commit

---
+++ header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>PHP Forms</title>
+  </head>
+  <body>
+    <main>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php require "header.php"; ?>
+<?php require "setting.php"; ?>
+<?php
+
+
+
+
+
+?>
+ <?php require "footer.php"; ?>
