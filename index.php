e4ed54a62ce06c61c44d6a4359c187a8564422c3
hermelen <hermelen.peris@gmail.com>
2018-08-07 10:15:53 +0200

    début intégration

--- index.php
+++ index.php
@@ -1,18 +1,45 @@
 <?php require "header.php"; ?>
 <?php require "setting.php"; ?>
 
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">My Mini CRM</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Listings <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Ajouter Client</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Ajouter Entreprise</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+
+
+<h1>Listing clients/entreprises</h1>
 
 
 
 
 
 <!-- Centered Tabs -->
-<ul class="nav nav-tabs nav-justified">
-  <li class="active"><a href="#">Client</a></li>
-  <li><a href="#">Entreprise</a></li>
-
+<ul class="nav nav-pills nav-fill">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Clients(3)</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Entreprise(4)</a>
+  </li>
 </ul>
-
 
 
 
@@ -28,10 +55,22 @@
         </button>
       </h5>
     </div>
-
     <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
       <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        <div class="row">
+          <div class="col-12 col-sm-4">
+            <img src="https://picsum.photos/200/200/?random" alt="">
+          </div>
+          <div class="col-12 col-sm-6">
+            <h3>Nom Prénom</h3>
+            <h4>Adresse du client, Code Postal, Ville</h4>
+            <h5>Nom de l'entreprise</h5>
+          </div>
+          <div class="col-12 col-sm-2">
+            <i class="fas fa-trash-alt"></i>
+            <i class="fas fa-edit"></i>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -39,13 +78,26 @@
     <div class="card-header" id="headingTwo">
       <h5 class="mb-0">
         <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Client #2
+          Client 2
         </button>
       </h5>
     </div>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
       <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        <div class="row">
+          <div class="col-12 col-sm-4">
+            <img src="https://picsum.photos/200/200/?random" alt="">
+          </div>
+          <div class="col-12 col-sm-6">
+            <h3>Nom Prénom</h3>
+            <h4>Adresse du client, Code Postal, Ville</h4>
+            <h5>Nom de l'entreprise</h5>
+          </div>
+          <div class="col-12 col-sm-2">
+            <i class="fas fa-trash-alt"></i>
+            <i class="fas fa-edit"></i>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -53,13 +105,26 @@
     <div class="card-header" id="headingThree">
       <h5 class="mb-0">
         <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Client #3
+          Client 3
         </button>
       </h5>
     </div>
     <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
       <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+        <div class="row">
+          <div class="col-12 col-sm-4">
+            <img src="https://picsum.photos/200/200/?random" alt="">
+          </div>
+          <div class="col-12 col-sm-6">
+            <h3>Nom Prénom</h3>
+            <h4>Adresse du client, Code Postal, Ville</h4>
+            <h5>Nom de l'entreprise</h5>
+          </div>
+          <div class="col-12 col-sm-2">
+            <i class="fas fa-trash-alt"></i>
+            <i class="fas fa-edit"></i>
+          </div>
+        </div>
       </div>
     </div>
   </div>
